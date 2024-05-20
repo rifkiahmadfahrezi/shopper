fe1162480f347ab1b319c6bb5a855c8e873088b0
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-20 19:48:25 +0700

    create home banner component

---
+++ src/components/fragment/HomeBanner.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import * as React from "react"
+import Autoplay from "embla-carousel-autoplay"
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+export default function HomeBanner() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true })
+  )
+
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full max-w-[80vw] h-[400px] mx-auto"
+      opts={{
+         loop: true
+      }}
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      
+      <CarouselPrevious />
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="h-[400px]" >
+            <div className="p-1 h-full">
+              <Card className="h-full">
+                <CardContent className="flex items-center justify-center p-6 h-full">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselNext />
+    </Carousel>
+  )
+}
