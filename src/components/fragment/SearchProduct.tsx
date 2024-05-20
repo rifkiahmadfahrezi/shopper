414929245e541f6da81f3105d0d66bebf3dcf9cc
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-20 19:49:18 +0700

    create search product modal component

---
+++ src/components/fragment/SearchProduct.tsx
@@ -0,0 +1,36 @@
+import {
+   Dialog,
+   DialogContent,
+   DialogDescription,
+   DialogHeader,
+   DialogTitle,
+   DialogTrigger,
+ } from "@/components/ui/dialog"
+ 
+ import { Input } from "../ui/input"
+ import { Button } from "../ui/button"
+ import { RiSearchLine } from "@remixicon/react"
+
+ 
+ export default function SearchProduct() {
+   return (
+      <Dialog>
+         <DialogTrigger>
+            <RiSearchLine size={24}/>
+         </DialogTrigger>
+         <DialogContent>
+            <DialogHeader>
+               <DialogTitle className="mb-7">Search some product...</DialogTitle>
+                 <form className="flex w-full items-center space-x-2">
+                  <Input type="search" placeholder="Shoes..." />
+                  <Button type="submit">
+                    <RiSearchLine size={16}/>
+                  </Button>
+                </form>
+            </DialogHeader>
+         </DialogContent>
+      </Dialog>
+
+   )
+ }
+
