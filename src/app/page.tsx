83df8c5ad6a0d47ed9692d246ece2cc34c1da381
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-21 19:56:50 +0700

    add product list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
 
 import HomeBanner from "@/components/fragment/HomeBanner"
+import ProductList from "@/components/fragment/ProductsList"
+import { getProduct } from "@/data/products"
+
 
 const HomePage : React.FC = ()  => {
+
+
   return (
     <>
       <div className="container mx-auto px-4">
         <HomeBanner />
+
+        <h1 className="text-center text-xl md:text-2xl font-semibold my-8 capitalize">Our products</h1>
+          <ProductList />
       </div>
     </>
   )
 }
-
 
 
 export default HomePage
