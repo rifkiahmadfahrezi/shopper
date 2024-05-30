fa5fe6ad5a3ab7e9cd12f451aa7c8eaf366b7823
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-31 06:58:11 +0700

    using useQuery to fetch data

--- src/app/(pages)/products/search/components/ProductResults.tsx
+++ src/app/(pages)/products/search/components/ProductResults.tsx
@@ -3,17 +3,16 @@
 import GridLayout from "@/components/layouts/GridLayout"
 import ProductCard from "@/components/ui/product-card" 
 import { useSearchParams } from "next/navigation"
-import useSearchProduct from "@/hooks/product/useSearchProduct"
-import type { Product } from "@/types/product"
 import ProductCardSkeleton from "@/components/skeletons/ProductCardSkeleton"
-import NProgress from "next-nprogress-bar"
 import MainContainer from "@/components/layouts/MainContainer"
-
+import type { Product } from "@/types/product"
+import { searchProduct } from "@/data/products"
+import { useQuery } from '@tanstack/react-query';
 
 export default function ProductResults() {
 
    const searchParams = useSearchParams()
-   const keyword = searchParams.get("q") || ""
+   const keyword = searchParams.get("q") ?? ""
 
    if(!keyword){
       return (
@@ -23,11 +22,11 @@ export default function ProductResults() {
       )
    }
 
-   const { data : products, isLoading, error } = useSearchProduct(keyword) ?? {}
-
-   if(products && !isLoading){
-      NProgress.stopProgress();
-   }
+   const { data: products, isLoading, error } = useQuery({
+      queryKey: ["products-result", keyword],
+      queryFn: () => searchProduct(keyword),
+      staleTime: 1000
+   });
 
    if(isLoading){
       return (
@@ -58,6 +57,12 @@ export default function ProductResults() {
 
    return <>
       <MainContainer>
+         
+         {products.length > 0 
+         ? <h1 className="my-8 text-lg">Results for "{keyword}" ({products?.length} items)</h1>
+         : <h1 className="my-8 text-lg">Product with keyword "{keyword}" is not exist!</h1>
+         }
+
          <GridLayout>
             {products && products.map((product : Product) => (
                <ProductCard key={product.id} product={product}/>
