6d8abfcdb4776d52ae129d9b29238a7311bdeb6b
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-31 06:58:56 +0700

    remove NProgress animation & set params to to url

--- src/components/forms/SearchProductForm.tsx
+++ src/components/forms/SearchProductForm.tsx
@@ -4,29 +4,33 @@ import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import { RiSearchLine } from "@remixicon/react"
 import { useState } from "react"
-import { useRouter, usePathname } from "next/navigation"
-import NProgress from "next-nprogress-bar"
+import { useSearchParams, useRouter, usePathname } from "next/navigation"
 
 export default function SearchProductForm() {
 
    const [keyword, setKeyword] = useState<string>("")
-   const router = useRouter()
-   const pathname = usePathname()
+
+   const { replace } = useRouter()
+   const pathname = usePathname();
+   const searchParams = useSearchParams()
 
    const handleSearch = async (e : React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-      NProgress.startProgress()
-      if(pathname != "/products/search"){
-         router.push(`/products/search?q=${keyword}`);
+      const params = new URLSearchParams(searchParams)
+      if(keyword){
+         params.set("q", keyword)
       }else{
-         router.replace(`/products/search?q=${keyword}`);
-         // router.refresh();
+         params.delete("q")
       }
+      replace(`/products/search?q=${keyword}`);
    }
 
    return (
       <form className="flex w-full items-center space-x-2" onSubmit={handleSearch}>
-         <Input type="search" onChange={(e) => {
+         <Input 
+            defaultValue={searchParams.get("q")?.toString()}
+            type="search" 
+            onChange={(e) => {
             setKeyword(e.target.value)
          }} placeholder="Shoes..." />
          <Button type="submit">
