b6510daa53f44fb4427e022e821a59704e29a2f5
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-05-30 15:42:06 +0700

    create shopper config

---
+++ src/lib/shopper.config.ts
@@ -0,0 +1,6 @@
+export const shopper = {
+   title: "Shopper",
+   description : "Discover the best products for your home and lifestyle. Shop our curated collection with confidence.",
+   tagline: "Effortless Shopping, Endless Choices",
+   keywords : "Shopper, online shop, online store, buy online, e-commerce, shopping, online shopping, fashion, electronics, home goods, beauty products, clothing, gadgets, accessories, free shipping, deals, discounts, sales, best prices, new arrivals, trendy, high-quality products"
+}
