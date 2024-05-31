0525566f16822f727265ae64180b9580b2af7ec1
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-06-01 05:45:39 +0700

    use react query infinite loading

--- src/app/(pages)/products/search/components/ProductResults.tsx
+++ src/app/(pages)/products/search/components/ProductResults.tsx
@@ -5,9 +5,12 @@ import ProductCard from "@/components/ui/product-card"
 import { useSearchParams } from "next/navigation"
 import ProductCardSkeleton from "@/components/skeletons/ProductCardSkeleton"
 import MainContainer from "@/components/layouts/MainContainer"
-import type { Product } from "@/types/product"
 import { searchProduct } from "@/data/products"
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from "@tanstack/react-query"
+import { useEffect } from "react"
+import { useInView } from "react-intersection-observer"
+import Link from "next/link"
+
 
 export default function ProductResults() {
 
@@ -17,27 +20,51 @@ export default function ProductResults() {
    if(!keyword){
       return (
          <div className="mt-8 container mx-auto text-xl text-center">
-            <h1>Please provide keywords for searching product!</h1>
+            <h1 className="text-lg mb-5">Please provide keywords for searching product!</h1>
+            <Link href={'/products'} className="btn btn-default text-sm">
+               View our products
+            </Link>
          </div>
       )
    }
 
-   const { data: products, isLoading, error } = useQuery({
-      queryKey: ["products-result", keyword],
-      queryFn: () => searchProduct(keyword),
-      staleTime: 1000
-   });
+   const { 
+      data, 
+      error, 
+      status, 
+      fetchNextPage , 
+      hasNextPage, 
+      isFetchingNextPage
+   } = useInfiniteQuery({
+         queryKey: ["products-search-result", keyword],
+         initialPageParam : 0,
+         queryFn: ({ pageParam = 0 }) => {
+            return searchProduct(keyword, pageParam, 30);
+         },
+         getNextPageParam: (lastPage) => {
+            return lastPage && lastPage.skip + lastPage.limit;
+         },
+      })
 
-   if(isLoading){
+   const { ref, inView } = useInView()
+
+   useEffect(() => {
+      if (inView && hasNextPage) {
+         fetchNextPage()
+      }
+   }, [hasNextPage, inView])
+
+
+   if(status === "pending"){
       return (
-      <> 
-      <MainContainer>
-         <GridLayout>
-            {Array(4).fill(0).map((_, i) => (
-               <ProductCardSkeleton key={i}/>
-            ))}
-         </GridLayout>
-      </MainContainer>
+      <>
+         <MainContainer>
+            <GridLayout>
+               {Array(4).fill(0).map((_, i) => (
+                  <ProductCardSkeleton key={i}/>
+               ))}
+            </GridLayout>
+         </MainContainer>
       </>
       )
    }
@@ -45,29 +72,42 @@ export default function ProductResults() {
    if(error){
       return (
       <>
-      <MainContainer>
-         <div className="bg-destructive">
+         <div className="bg-destructive text-center p-5">
             <h1>Failed to fetch data : {error.message}</h1>
-            <p>{error.stack}</p>
          </div>
-      </MainContainer>
       </>
       )
    }
 
-   return <>
+
+   return (
+      <>
       <MainContainer>
-         
-         {products.length > 0 
-         ? <h1 className="my-8 text-lg">Results for "{keyword}" ({products?.length} items)</h1>
-         : <h1 className="my-8 text-lg">Product with keyword "{keyword}" is not exist!</h1>
-         }
+
+         <h1 className="text-lg my-7">Results for "{keyword}" ({data.pages[0]?.total} items)</h1>
 
          <GridLayout>
-            {products && products.map((product : Product) => (
-               <ProductCard key={product.id} product={product}/>
-            ))}
+            {data.pages.map((page) => (
+               page?.products.map((product, index, arr) => (
+                  <div ref={arr.length - 1 === index ? ref : null}>
+                     <ProductCard key={product.id} product={product}/>
+                  </div>
+               )
+            )))}
          </GridLayout>
+         <GridLayout>
+            {(isFetchingNextPage) &&
+               Array(4).fill(0).map((_, i) => (
+                  <ProductCardSkeleton key={i}/>
+               ))
+            }
+         </GridLayout>
+
+            {(data.pages[0]?.total ===  data.pages[data.pages.length - 1]?.skip) && 
+               <span className="text-center flex justify-center my-7">All data loaded</span>
+            }
+
       </MainContainer>
-   </>
+      </>
+   )
 }
--- src/components/fragment/Products.tsx
+++ src/components/fragment/Products.tsx
@@ -1,23 +1,45 @@
 "use client"
 
-import useProduct from "@/hooks/product/useProduct"
+// import useProduct from "@/hooks/product/useProduct"
 import ProductCardSkeleton from "../skeletons/ProductCardSkeleton"
 import ProductCard from "../ui/product-card"
 import GridLayout from "../layouts/GridLayout"
-import { type Product } from "@/types/product"
-// import { useInfiniteQuery } from "@tanstack/react-query"
-// import { getProductsWithPaging } from "@/data/products"
+import { useInView } from "react-intersection-observer"
+import { getProductsWithPaging } from "@/data/products"
+import { useEffect, useState } from "react"
+import { useInfiniteQuery } from "@tanstack/react-query"
 
 export default function Products() {
 
-   const { data, isLoading, error } = useProduct()
+   const { 
+      data, 
+      error, 
+      status, 
+      fetchNextPage , 
+      hasNextPage, 
+      isFetchingNextPage
+   } = useInfiniteQuery({
+         queryKey: ["products"],
+         initialPageParam : 0,
+         queryFn: ({ pageParam = 0 }) => {
+            return getProductsWithPaging(pageParam, 30);
+         },
+         getNextPageParam: (lastPage) => {
+            return lastPage && lastPage.skip + lastPage.limit;
+         },
+      })
 
-   // const { data}  = useInfiniteQuery({
-   //    queryKey: ["products-with-pages"],
-   //    queryFn: () => getProductsWithPaging
-   // })
+   const { ref, inView } = useInView()
 
-   if(isLoading){
+
+   useEffect(() => {
+      if (inView && hasNextPage) {
+         fetchNextPage()
+      }
+   }, [inView, hasNextPage])
+
+
+   if(status === "pending"){
       return (
       <>
          <GridLayout>
@@ -32,22 +54,37 @@ export default function Products() {
    if(error){
       return (
       <>
-         <div className="bg-destructive">
+         <div className="bg-destructive text-center p-5">
             <h1>Failed to fetch data : {error.message}</h1>
-            <p>{error.stack}</p>
          </div>
       </>
       )
    }
 
 
+
    return (
       <>
          <GridLayout>
-            {data && data.map((product : Product) => (
-               <ProductCard key={product.id} product={product}/>
-            ))}
+            {data.pages.map((page) => (
+               page?.products.map((product, index, arr) => (
+                  <div ref={arr.length - 1 === index ? ref : null}>
+                     <ProductCard key={product.id} product={product}/>
+                  </div>
+               )
+            )))}
          </GridLayout>
+         <GridLayout>
+            {(isFetchingNextPage) &&
+               Array(4).fill(0).map((_, i) => (
+                  <ProductCardSkeleton key={i}/>
+               ))
+            }
+         </GridLayout>
+
+         {(data.pages[0]?.total ===  data.pages[data.pages.length - 1]?.skip) && 
+               <span className="text-center flex justify-center my-7">All data loaded</span>
+            }
       </>
    )
 }
