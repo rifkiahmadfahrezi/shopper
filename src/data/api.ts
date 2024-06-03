72ba883dc70fbcb4707866a6d4fcc64a70757eab
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-06-03 18:33:09 +0700

    add auth api

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,3 +3,11 @@ import axios from "axios";
 export const myShop = axios.create({
    baseURL: process.env.API_BASE,
 })
+
+export const authApi = async (token : string) => {
+   return myShop.get("/auth/me", {
+      headers: {
+         "Authorization": `Bearer ${token}`
+      }
+   })
+}
