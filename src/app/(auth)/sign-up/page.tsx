868fe1074f899db4268d6bec4f6de8e89b59f82d
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-06-05 05:25:41 +0700

    create sigIn form

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,58 +6,67 @@ import { RiGoogleFill } from "@remixicon/react"
 import { Grocery1 } from "@/components/icons"
 import { Metadata } from "next"
 
+import { 
+  AuthContent, 
+  AuthFormContainer, 
+  AuthImage, 
+  AuthLayout, 
+  AuthTitle } from "@/components/layouts/AuthLayout"
+
+import { FormEvent } from "react"
+
 export const metadata : Metadata = {
   title: "Sign up - start your journey here"
-}
+} 
 
 
 export default function Component() {
+
+  const handleSignIn =  (event : FormEvent) => {
+    event.preventDefault()
+  } 
+
   return (
-   <div className=" container mx-auto px-5">
-      <div className="grid min-h-screen w-full grid-cols-1 lg:grid-cols-2 place-items-center">
-      <div className="relative hidden lg:block">
-         <Grocery1 />
-      </div>
-      <div className="flex items-center w-full max-w-lg justify-center p-6 sm:p-12 lg:p-16">
-        <div className="w-full max-w-md space-y-6">
-          <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold">Sign Up</h1>
-            <p className="text-gray-500 dark:text-gray-400">Create your account to get started.</p>
-            
-          </div>
-          <form className="space-y-4">
-            <div>
-              <Label htmlFor="email">Email</Label>
-              <Input id="email" placeholder="Enter your email" required type="email" />
-            </div>
-            <div>
-              <Label htmlFor="password">Password</Label>
-              <Input id="password" placeholder="Enter your password" required type="password" />
-            </div>
-            <div>
-              <Label htmlFor="password">Confirm Password</Label>
-              <Input id="password" placeholder="Confirm your password" required type="password" />
-            </div>
-            <Button className="w-full" type="submit">
-              Sign Up
-            </Button>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-white px-2 text-gray-500 dark:bg-gray-950 dark:text-gray-400">Or sign up with</span>
-              </div>
-            </div>
-            <Button className="w-full" variant="outline">
-              <RiGoogleFill className="mr-2 h-4 w-4" />
-              Sign up with Google
-            </Button>
-          </form>
-        </div>
-      </div>
-    </div>
-   </div>
-    
+    <AuthLayout>
+      <AuthImage>
+        <Grocery1 />
+      </AuthImage>
+      <AuthContent>
+        <AuthTitle title="Sign up">
+          <p className="text-gray-500 dark:text-gray-400">Create your account to get started.</p>
+        </AuthTitle>
+        <AuthFormContainer>
+           <form className="space-y-4">
+             <div>
+               <Label htmlFor="email">Email</Label>
+               <Input id="email" name="email" placeholder="Enter your email" required type="email" />
+             </div>
+             <div>
+               <Label htmlFor="password">Password</Label>
+               <Input id="password" name="password" placeholder="Enter your password" required type="password" />
+             </div>
+             <div>
+               <Label htmlFor="password">Confirm Password</Label>
+               <Input id="password" name="confirm-password" placeholder="Confirm your password" required type="password" />
+             </div>
+             <Button className="w-full" type="submit">
+               Sign Up
+             </Button>
+             <div className="relative">
+               <div className="absolute inset-0 flex items-center">
+                 <span className="w-full border-t" />
+               </div>
+               <div className="relative flex justify-center text-xs uppercase">
+                 <span className="bg-white px-2 text-gray-500 dark:bg-gray-950 dark:text-gray-400">Or sign up with</span>
+               </div>
+             </div>
+             <Button className="w-full" variant="outline">
+               <RiGoogleFill className="mr-2 h-4 w-4" />
+               Sign up with Google
+             </Button>
+           </form>
+        </AuthFormContainer>
+      </AuthContent>
+    </AuthLayout>
   )
 }
