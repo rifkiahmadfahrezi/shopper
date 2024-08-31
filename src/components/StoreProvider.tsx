808a061c060a31f136feafe5de5954b08d337b6a
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-08-31 07:07:49 +0700

    create redux provider

---
+++ src/components/StoreProvider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Provider } from "react-redux"
+import { makeStore, type AppStore } from "@/store/store"
+import { useRef } from "react"
+
+export default function StoreProvider({ children } : { children : React.ReactNode}) {
+   const storeRef = useRef<AppStore>()
+
+   if(!storeRef.current){
+      storeRef.current = makeStore()
+   }
+
+   return (
+      <Provider store={storeRef.current}>
+         {children}
+      </Provider>
+   )
+}
