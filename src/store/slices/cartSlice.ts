4698032260b318f26e73bb9a01c27e44b93715bb
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2024-09-02 09:17:02 +0700

    fix redux persist issue

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -1,10 +1,12 @@
+'use client'
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 import type { Cart, CartItem } from "@/types/cart";
 
 const initialState : Cart = {
    products: [],
-   totalPrice: 0
+   totalPrice: 0,
+   totalItem: 0
 }
 
 const CartSlice = createSlice({
@@ -21,20 +23,35 @@ const CartSlice = createSlice({
          }
      
          state.totalPrice += action.payload.price;
+         state.totalItem += 1
       },     
+      changeAmountByNumber: (state, action : PayloadAction<{ id : number | string, amount : number }>) => {
+         const item = state.products.find(product => product.id === action.payload.id)
+
+         if(!item){
+            alert('Product not found')
+         }else{
+            item.amount = action.payload.amount
+         }
+
+      },
       removeCartItem: (state, action: PayloadAction<CartItem>) => {
          const item = state.products.find(item => item.id === action.payload.id)
          if(!item){
-            state.products.filter(item => item.id !== action.payload.id)
+            state.products = state.products.filter(item => item.id !== action.payload.id)
          }else{
-            state.products = [...state.products, { ...item, amount: item.amount -= 1 }]
+            if(item.amount <= 1){
+               state.products = state.products.filter(item => item.id !== action.payload.id)
+            }else{
+               item.amount -= 1
+            }
+            state.totalPrice -= action.payload.price;
+            state.totalItem -= 1
          }
-         state.totalPrice -= action.payload.price;
       },
-
    }
 })
 
 
-export const { addCartItem, removeCartItem } = CartSlice.actions
+export const { addCartItem, removeCartItem, changeAmountByNumber } = CartSlice.actions
 export default CartSlice.reducer
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,18 @@
+"use client"
+
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import CartSlice from './slices/cartSlice'
-import { persistStore, persistReducer, } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import {
+   persistStore,
+   persistReducer,
+   FLUSH,
+   REHYDRATE,
+   PAUSE,
+   PERSIST,
+   PURGE,
+   REGISTER,
+ } from 'redux-persist'
+import storage from './storage'
 
 const rootReducer = combineReducers({
    cart:CartSlice
@@ -17,7 +28,14 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const makeStore = () => {
    return configureStore({
-      reducer: persistedReducer
+      devTools: true,
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+         getDefaultMiddleware({
+            serializableCheck: {
+               ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+      }),
    })
 }
 
